11a1da642b33495242e23d30f85a2b10864e9c4f
Makusu <vloxomire@gmail.com>
2018-08-09 20:10:18 -0300

    inicio
    creacion

---
+++ app/src/main/java/com/example/itmasterprofessionaltraining/autosmvc/Interfaz/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.itmasterprofessionaltraining.autosmvc.Interfaz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.itmasterprofessionaltraining.autosmvc.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

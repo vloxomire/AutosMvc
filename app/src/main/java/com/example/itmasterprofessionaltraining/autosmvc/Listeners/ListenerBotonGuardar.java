367cf36ac533932eb584d09092d1e1f3f3824ea8
Makusu <vloxomire@gmail.com>
2018-08-09 21:56:19 -0300

    ControladorAuto
    se termino las clases del controlador auto

--- app/src/main/java/com/example/itmasterprofessionaltraining/autosmvc/Listeners/ListenerBotonGuardar.java
+++ app/src/main/java/com/example/itmasterprofessionaltraining/autosmvc/Listeners/ListenerBotonGuardar.java
@@ -1,4 +1,25 @@
 package com.example.itmasterprofessionaltraining.autosmvc.Listeners;
 
-public class ListenerBotonGuardar {
+import android.view.View;
+
+import com.example.itmasterprofessionaltraining.autosmvc.Interfaz.MainActivity;
+import com.example.itmasterprofessionaltraining.autosmvc.Models.Auto;
+
+public class ListenerBotonGuardar implements View.OnClickListener{
+    MainActivity context;
+
+
+    public ListenerBotonGuardar(MainActivity context) {
+        this.context = context;
+    }
+
+    @Override
+    public void onClick(View view) {
+        context.setUnAuto(new Auto());
+        context.getUnAuto().setModelo(context.geteTextModelo().getText().toString());
+        context.getUnAuto().setMarca(context.geteTextMarca().getText().toString());
+        context.getUnAuto().setAnio(Integer.valueOf(context.geteTextAnio().getText().toString()));
+        context.getUnAuto().setKm(Integer.valueOf(context.geteTextKm().getText().toString()));
+
+    }
 }
